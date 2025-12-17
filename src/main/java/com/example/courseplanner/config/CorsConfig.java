e23c9df40aa82007cb5e80feb9b6f5ea073782ad
ChaitanyaMittal27 <chaitanyamittal27@gmail.com>
2025-12-17 14:55:03 -0800

    modified cors permission to allow vercel

--- src/main/java/com/example/courseplanner/config/CorsConfig.java
+++ src/main/java/com/example/courseplanner/config/CorsConfig.java
@@ -9,7 +9,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:3000") // Next.js dev server
+                .allowedOrigins("http://localhost:3000", "https://sfu-course-planner.vercel.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*");
     }
